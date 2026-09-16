6912ae0664e5178d974a19f7180fba81e341faf6
OSSJU <Junior.sereia@hotmail.com>
2026-09-16 16:11:00 -0400

    chore: export own chat domain

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,6 +1,7 @@
 export const PROJECT_NAME = 'Caneca Fácil';
 
 export * from './briefing';
+export * from './chat';
 export * from './conversation';
 export * from './customer';
 export * from './phone';
