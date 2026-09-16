d486d93589c098aeea2a4896f4132915f41b0b38
OSSJU <Junior.sereia@hotmail.com>
2026-09-16 19:05:11 -0400

    fix: emit NodeNext-safe core package exports

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,8 +1,8 @@
 export const PROJECT_NAME = 'Caneca Fácil';
 
-export * from './briefing';
-export * from './chat';
-export * from './conversation';
-export * from './customer';
-export * from './phone';
-export * from './project-status';
+export * from './briefing.js';
+export * from './chat.js';
+export * from './conversation.js';
+export * from './customer.js';
+export * from './phone.js';
+export * from './project-status.js';
