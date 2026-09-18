5bc61890f3305caabe2d92a207f4ed47e3a8e711
OSSJU <Junior.sereia@hotmail.com>
2026-09-18 19:57:41 -0400

    feat: export chat component protocol

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -2,6 +2,7 @@ export const PROJECT_NAME = 'Caneca Fácil';
 
 export * from './briefing';
 export * from './chat';
+export * from './chat-components';
 export * from './conversation';
 export * from './customer';
 export * from './phone';
